dea8edc44c1afc18403b12664b8d7d84ce6f4cb9
Levi <levifernandes@usp.br>
2019-11-20 18:12:24 -0300

    terminando o controller do produto

--- app/controllers/product.js
+++ app/controllers/product.js
@@ -1,22 +1,37 @@
 const Product = require('./../models/Product')
 
-async function getAllUsers(req, res) {
-  User.getAllUsers((err, results) => {
+async function addProduct(req, res, product) {
+  Product.addProduct(product, (err, results) => {
     if(err) res.send({status: 'error'})
     else res.send(results)
   })
 }
 
-async function me(req, res) {
-  User.getUserbyID(res.locals.id, (err, result) => {
-    if(err || result.length == 0) res.status(404).send('user not found')
-    else {
-      res.status(200).send(result)
-    }
+async function getAllProducts(req, res) {
+  Product.getAllProducts((err, results) => {
+    if(err) res.send({status: 'error'})
+    else res.send(results)
   })
 }
 
+async function getProductbyID(req, res, id) {
+  Product.getProductbyID(id, (err, results) => {
+    if(err) res.send({status: 'error'})
+    else res.send(results)
+  })
+}
+
+async function findProductByCategory(req, res, category) {
+  Product.findProductByCategory(category, (err, results) => {
+    if(err) res.send({status: 'error'})
+    else res.send(results)
+  })
+}
+
+
 module.exports = {
-  getAllUsers,
-  me
+  getAllProducts,
+  getProductbyID,
+  findProductByCategory,
+  addProduct
 }
