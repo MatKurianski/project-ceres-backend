255fe92d18fa6fea0e8b188a8e26ec05d0772081
Matheus Kurianski <matheus1340@hotmail.com>
2019-11-24 01:10:59 -0300

    status online

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,11 @@
 const user = require('./user')
 const auth = require('./auth')
 const product = require('./product')
-
+const online = require('./online_users')
 
 module.exports = function(app) {
   user(app)
   product(app)
-  auth(app)
+  auth(app),
+  online(app)
 }
